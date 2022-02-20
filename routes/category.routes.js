5b3130ea13d74badfcb7c5c9993280e55d13b124
SnuggleTrouble <stabjo@outlook.com>
2022-02-20 09:45:49 +0100

    add category folder with ejs file inside of views. Edit category routes. You can now create categories from the website, but must type localhost:3000/category/create to get to page.

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -3,11 +3,11 @@ const Category = require("../models/category.model");
 
 const router = express.Router();
 
-router.get("/", (req, res) => {
+router.get("/create", (req, res) => {
   res.render("category/create");
 });
 
-router.post("/", async (req, res) => {
+router.post("/create", async (req, res) => {
   await Category.create({
     name: req.body.name,
   });
