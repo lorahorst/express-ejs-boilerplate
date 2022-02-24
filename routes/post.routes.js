bf9147373fbe02dd64fa31e2f118c214b7c1c678
SnuggleTrouble <stabjo@outlook.com>
2022-02-24 22:20:29 +0100

    add method overrride to server.js.  modify post model to include description. modify ejs files with a post form that refers users wanting to create or edit posts. change post routes. So far there's a bug with the file upload, as it is undefined inside its current route; this needs to be fixed

--- routes/post.routes.js
+++ routes/post.routes.js
@@ -7,34 +7,48 @@ const fileUploader = require("../cloudinary.config");
 const router = express.Router();
 
 // The post creation form
-router.get("/create", isLoggedIn, async (req, res) => {
+router.get("/createPost", isLoggedIn, async (req, res) => {
+  const post = new Post();
   const categories = await Category.find();
-  res.render("post/create", { categories });
+  res.render("post/createPost", { post, categories });
 });
 
 // The post creation handler
 router.post(
-  "/create",
+  "/createPost",
   isLoggedIn,
   fileUploader.single("file"),
-  async (req, res) => {
-    console.log(req.file);
-    const post = new Post();
-    post.title = req.body.title;
-    post.content = req.body.content;
-    post.image = req.file.path;
-    post.imageName = req.file.originalname;
-    post.private = req.body.private;
-    post.category = req.body.category;
-    post.author = req.session.currentUser._id;
-    try {
-      await post.save();
-      res.redirect("/");
-    } catch (error) {
-      res.redirect("/post/create");
-    }
-  }
+  async (req, res, next) => {
+    req.post = new Post();
+    req.file.path;
+    req.file.originalname;
+    next();
+  },
+  createPostAndRedirect("createPost")
 );
+
+// form for updating an existing post
+router.get("/editPost/:id", isLoggedIn, async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  const categories = await Category.find();
+  res.render("post/editPost", { post, categories });
+});
+
+// route for handling the update of an existing post
+router.put(
+  "/:id",
+  async (req, res, next) => {
+    req.post = await Post.findById(req.params.id);
+    next();
+  },
+  createPostAndRedirect("editPost")
+);
+
+// route for handling the deletion of a post
+router.delete("/:id", isLoggedIn, async (req, res) => {
+  await Post.findByIdAndDelete(req.params.id);
+  res.redirect("/post/myJournal");
+});
 
 // Shows all posts
 router.get("/myJournal", isLoggedIn, async (req, res) => {
@@ -69,5 +83,23 @@ router.get("/:id", isLoggedIn, async (req, res) => {
     });
   res.render("post/viewOne", { post });
 });
+
+function createPostAndRedirect(template) {
+  return async (req, res) => {
+    console.log(req.file);
+    req.post.title = req.body.title;
+    req.post.description = req.body.description;
+    req.post.content = req.body.content;
+    req.post.private = req.body.private;
+    req.post.category = req.body.category;
+    req.post.author = req.session.currentUser._id;
+    try {
+      await req.post.save();
+      res.redirect("/");
+    } catch (error) {
+      res.render(template, { post: req.post });
+    }
+  };
+}
 
 module.exports = router;
