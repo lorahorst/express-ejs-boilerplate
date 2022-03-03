ddfbfc162d465ce8ff25e2756883a9df5f3d2770
SnuggleTrouble <stabjo@outlook.com>
2022-03-03 20:51:14 +0100

    modify lots of things to make things look better

--- routes/category.routes.js
+++ routes/category.routes.js
@@ -1,13 +1,14 @@
 const express = require("express");
+const { isLoggedIn } = require("../middlewares/guard");
 const Category = require("../models/category.model");
 
 const router = express.Router();
 
-router.get("/create", (req, res) => {
+router.get("/create", isLoggedIn, (req, res) => {
   res.render("category/create");
 });
 
-router.post("/create", async (req, res) => {
+router.post("/create", isLoggedIn, async (req, res) => {
   await Category.create({
     name: req.body.name,
   });
